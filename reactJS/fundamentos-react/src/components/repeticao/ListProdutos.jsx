7e5dee98eea1ac5a052dcd09a623d16524d80abe
Gabr1el94 <bielsystem64@gmail.com>
2020-10-18 16:07:29 -0300

    change format attribute "preco"

--- reactJS/fundamentos-react/src/components/repeticao/ListProdutos.jsx
+++ reactJS/fundamentos-react/src/components/repeticao/ListProdutos.jsx
@@ -9,7 +9,7 @@ export default (props) => {
             <tr key={produto.id} className={i % 2 === 0 ? 'Par' : 'Impar'}>
                 <td>{produto.id}</td>
                 <td>{produto.nome}</td>
-                <td>R$ {produto.preco.toFixed(2).substring('.',',')}</td>
+                <td>R$ {produto.preco.toFixed(2)}</td>
             </tr>
         );
     });
