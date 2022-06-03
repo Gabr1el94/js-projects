c97e4253f24d1f6f66bdb8d313fc34930a434550
Gabr1el94 <bielsystem64@gmail.com>
2022-06-03 10:57:29 -0300

    prototype insert colors in countries done

--- map-global-interactive/js/script.js
+++ map-global-interactive/js/script.js
@@ -1,5 +1,20 @@
+var btcAccept = ['SV','CF'];
+var btcBanid = ['DZ','MA','TN','EG','IQ','NP','QA','OM','BO','BD','MK','CN'];
+var btcFriendly = [];
+var elementsPath = document.getElementsByTagName('path');
 
-
-addEventListener('mouseover', e => {
-    
+addEventListener('load', e => {
+    playCountries(btcAccept, '#FFC93C');
+    playCountries(btcBanid, '#BE3144');
+    playCountries(btcFriendly, '#007FFF');  
 }, false);
+
+function playCountries(type, color) {
+    for (let i = 0; i < elementsPath.length; i++) {
+        for (let j = 0; j < type.length; j++) {
+            if (elementsPath[i].id == type[j]) {
+                elementsPath[i].style.fill = color;
+            }
+        }
+    }
+}
