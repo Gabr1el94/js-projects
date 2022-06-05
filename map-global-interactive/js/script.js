4cbdb2265cfefc8c751548680b1c347ef1d53cb9
Gabr1el94 <bielsystem64@gmail.com>
2022-06-05 04:08:32 -0300

    insert title in countries done

--- map-global-interactive/js/script.js
+++ map-global-interactive/js/script.js
@@ -1,13 +1,24 @@
 var btcAccept = ['SV','CF'];
-var btcBanid = ['DZ','MA','TN','EG','IQ','NP','QA','OM','BO','BD','MK','CN'];
-var btcFriendly = [];
+var btcBanid = ['DZ','BO','CN','CO','EG','ID','IR','IN','IR','XK','NP','RU','TR','VN'];
+var btcFriendly = ['BR','PY','PA','EE'];
 var elementsPath = document.getElementsByTagName('path');
 
 addEventListener('load', e => {
+    setterTitles();
     playCountries(btcAccept, '#FFC93C');
     playCountries(btcBanid, '#BE3144');
     playCountries(btcFriendly, '#007FFF');  
 }, false);
+
+function setterTitles(){
+    for (let i = 0; i < elementsPath.length; i++) {
+        let tagTitle = elementsPath[i].getElementsByTagName('title');
+        for (let index = 0; index < tagTitle.length; index++) {
+            let title = tagTitle[index];
+            title.innerHTML=elementsPath[i].getAttribute("name");
+        }
+    }
+}
 
 function playCountries(type, color) {
     for (let i = 0; i < elementsPath.length; i++) {
