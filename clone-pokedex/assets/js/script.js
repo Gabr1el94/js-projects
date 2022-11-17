8c4388b6db1860eb540dfae315e3fe2643a688a9
Gabr1el94 <bielsystem64@gmail.com>
2022-11-17 17:45:43 -0300

    fetch pokemon list

--- clone-pokedex/assets/js/script.js
+++ clone-pokedex/assets/js/script.js
@@ -3,6 +3,7 @@ const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 fetch(url)
-.then(function (res){
-    console.log(res);
-});
+    .then((response)=> response.json())
+    .then((jsonBody => consol.log(jsonBody)))
+    .catch((err => console.log(err)))
+    .finally(() => console.log('Requisição concluída!'));
