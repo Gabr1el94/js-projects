2bcca62fdbe5c1a864b8f32cedd530b7e814d293
Gabr1el94 <bielsystem64@gmail.com>
2022-11-20 19:37:42 -0300

    convert pokemons to items of html on JS

--- clone-pokedex/assets/js/script.js
+++ clone-pokedex/assets/js/script.js
@@ -1,9 +1,23 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response)=> response.json())
-    .then((jsonBody => consol.log(jsonBody)))
-    .catch((err => console.log(err)))
-    .finally(() => console.log('Requisição concluída!'));
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <span class="number">001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                alt="${pokemon.name}">
+        </div>
+    </li>`;
+}
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML += newHtml
+}).finally(() => console.log('Requisição concluída!'));
