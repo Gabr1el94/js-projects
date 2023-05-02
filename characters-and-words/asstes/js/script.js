79d0df8c885c1f76ca9531551d74e749b840d20b
Gabr1el94 <bielsystem64@gmail.com>
2023-05-02 18:19:03 -0300

    add exercise count caracters and words

---
+++ characters-and-words/asstes/js/script.js
@@ -0,0 +1,32 @@
+//Actions count caracters and words
+
+const input = document.querySelector("#input");
+const counter = document.querySelector("#counter");
+const toggleButton = document.querySelector("#toggle");
+
+let mode = "caracters";
+
+toggleButton.addEventListener("click", () => {
+    if (mode === "caracters") {
+        mode = "words";
+        toggleButton.textContent = "Count caracters";
+    } else {
+        mode = "caracters";
+        toggleButton.textContent = "Count words";
+    }
+
+    input.dispatchEvent(new Event("input"));
+})
+
+input.addEventListener("input", () => {
+    let count = 0;
+
+    if (mode === "characters") {
+        count = input.value.length;
+        counter.textContent = `${count} caracters(s)`;
+    } else {
+        const words = input.value.trim().split(/\s+/);
+        count = input.value.trim() === "" ? 0 : words.length;
+        counter.textContent = `${count} word(s)`;
+    }
+})
